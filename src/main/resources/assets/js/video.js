7219fa87a00e8d4b68734ded8ce0d43aec78951e
ehodges <edhodges@gmail.com>
2013-02-28 07:22:00 +0000

    Re-organising templates with includes.

---
+++ src/main/resources/assets/js/video.js
@@ -0,0 +1,34 @@
+var lmsObject;
+
+hasGetUserMedia = function() {
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+myGetUserMedia = function() {
+    return (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+onGotStream = function(stream) {
+    var url = webkitURL.createObjectURL(stream);
+    video.src = url;
+    lmsObject = stream;
+    $("#stopButton").prop("disabled", false);
+    $("#startButton").prop("disabled", true);
+}
+
+onFailedStream = function(stream) {
+}
+
+startTest = function() {
+    navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+    navigator.getUserMedia({video:true}, onGotStream, onFailedStream);
+}
+
+stopTest = function() {
+    video.pause();
+    lmsObject.stop();
+    $("#stopButton").prop("disabled", true);
+    $("#startButton").prop("disabled", false);
+}
